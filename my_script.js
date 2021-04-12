57211062713e8149d2a3c85bfe0109be710028e2
Ciarm-nik <nikcia@libero.it>
2021-04-12 15:03:35 +0200

    Inizio esercizio

---
+++ my_script.js
@@ -0,0 +1,19 @@
+// Creo un ciclo per stampare un numero da 1 a 100
+for (var i=1; i < 100 + 1; i++) {
+
+    if(i % 3 ===0) {
+    console.log("Fizz")    
+    }
+    if(i % 5 ===0) {
+        console.log("Buzz")
+    } 
+    // numero inserito dall'utente
+   console.log("Il numero estratto è: " + i)
+
+}
+// Per i multipli di 3 stampo Fizz
+
+// Per i multipli di 5 stampo Buzz
+
+// Per i multipli di 3 e 5 stampo FizzBuzz
+
